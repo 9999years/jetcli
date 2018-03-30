8041b49f488bf414a1cd17923c70878aed1d8702
Rebecca Turner <637275@gmail.com>
2018-03-30 13:10:20 -0400

    failing tests

--- jet/jet.go
+++ jet/jet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"flag"
 	"bytes"
 	"errors"
@@ -9,6 +10,22 @@ import (
 	"github.com/CloudyKit/jet"
 )
 
+// exists returns whether the given file or directory exists or not
+// https://stackoverflow.com/a/10510783/5719760
+// returns true, nil if the file exists and false, nil if the file doesn't
+// exist and a non-nil error for other Stat errors like a permissions error
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// render renders a template
 func render(directory string, templateName string) (string, error) {
 	view := jet.NewHTMLSet(directory)
 	tpl, err := view.GetTemplate(templateName)
@@ -25,17 +42,38 @@ func render(directory string, templateName string) (string, error) {
 
 func parseArgs() (string, string, error) {
 	set := flag.NewFlagSet("", flag.ContinueOnError)
-	directory := set.String("directory", "./", "The directory to search for templates in")
-	err := set.Parse(os.Args[1:])
-	if set.NArg() != 1 {
-		return "", "", errors.New("One filename of a template to render required!")
+	set.Usage = func() {
+		argspec := "[options] TEMPLATE_NAME"
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s %s\n",
+			os.Args[0], argspec)
+		set.PrintDefaults()
 	}
-	templateName := set.Arg(0)
-	return *directory, templateName, err
+	directory := set.String("dir", "./", "The directory to search for templates in")
+	const templateArg = "template"
+	templateName := set.String(templateArg, "./", "The filename of the template to render")
+	err := set.Parse(os.Args[1:])
+	if err != nil {
+		return "", "", err
+	}
+	if set.Lookup(templateArg) == nil {
+		if set.NArg() != 1 {
+			return "", "", errors.New("One filename of a template to render required!")
+		} else {
+			// template name given positionally
+			// hack to use string as *string
+			tmp := set.Arg(0)
+			templateName = &tmp
+		}
+	}
+	return *directory, *templateName, err
 }
 
 func main() {
 	directory, templateName, err := parseArgs()
+	if err == flag.ErrHelp {
+		// just printed the help msg, not an error
+		os.Exit(-1)
+	}
 	if err != nil {
 		os.Stderr.WriteString("Illegal arguments: " + err.Error())
 		os.Exit(-1)
--- jet/jet_test.go
+++ jet/jet_test.go
@@ -5,21 +5,23 @@ import (
 	"testing"
 )
 
-type TestCase struct {
+type RenderTestCase struct {
 	Args []string
 	Expected string
 	ArgErr bool
 	RenderErr bool
 }
 
-func mockedArgs(t *testing.T, test TestCase) {
+// tests rendering
+func testRender(t *testing.T, test RenderTestCase) {
 	_Args := os.Args
-	os.Args = append([]string{"jetcli"}, test.Args...)
+	os.Args = append([]string{"jet"}, test.Args...)
 	tpl, dir, err := parseArgs()
 	if err != nil {
 		if !test.ArgErr {
 			// unexpected error
-			t.Errorf("Incorrect argument error for %v; Expected: %v but got %v", test.Args, test.ArgErr, err)
+			t.Errorf("Incorrect argument error for %v; Expected no error but got %v",
+				test.ArgErr, err)
 		} else {
 			// expected error
 			return
@@ -28,46 +30,112 @@ func mockedArgs(t *testing.T, test TestCase) {
 	rendered, err := render(tpl, dir)
 	if err != nil {
 		if !test.RenderErr {
-			t.Errorf("Incorrect render error for %v; Expected: %v but got %v", test.Args, test.RenderErr, err)
+			t.Errorf("Incorrect render error for %v; Expected no error but got %v",
+				test.RenderErr, err)
 		} else {
 			return
 		}
 	}
 	if rendered != test.Expected {
-		t.Errorf("Failed for %v; Expected: `%v` but got `%v`", test.Args, test.Expected, rendered)
+		t.Errorf("Failed for %v; Expected: `%v` but got `%v`",
+			test.Args, test.Expected, rendered)
 	}
 	os.Args = _Args
 }
 
 func TestBasic(t *testing.T) {
-	mockedArgs(t, TestCase{
-		Args: []string{"-directory", "testdata", "test.html"},
+	testRender(t, RenderTestCase{
+		Args: []string{"-dir", "testdata", "test.html"},
 		Expected: "title: hello from the jet CLI\nbody:  default body\n",
 		ArgErr: false,
 		RenderErr: false,
 	})
-	mockedArgs(t, TestCase{
+	testRender(t, RenderTestCase{
 		Args: []string{"./testdata/test.html"},
 		Expected: "title: hello from the jet CLI\nbody:  default body\n",
 		ArgErr: false,
 		RenderErr: false,
 	})
-	mockedArgs(t, TestCase{
+	testRender(t, RenderTestCase{
 		Args: []string{"./testdata/nonexistent"},
 		Expected:  "",
 		ArgErr: false,
 		RenderErr: true,
 	})
-	mockedArgs(t, TestCase{
+	testRender(t, RenderTestCase{
 		Args: []string{},
 		Expected:  "",
 		ArgErr: true,
 		RenderErr: false,
 	})
-	mockedArgs(t, TestCase{
+	testRender(t, RenderTestCase{
 		Args: []string{"x", "y"},
 		Expected:  "",
 		ArgErr: true,
 		RenderErr: false,
+	})
+}
+
+type ArgTestCase struct {
+	Args []string
+	Template string
+	Directory string
+	Err bool
+}
+
+func testArgs(t *testing.T, test ArgTestCase) {
+	_Args := os.Args
+	os.Args = append([]string{"jet"}, test.Args...)
+	dir, tpl, err := parseArgs()
+	if err != nil {
+		if !test.Err {
+			// unexpected error
+			t.Errorf("Unexpected error: Expected none but got %s", err)
+		} else {
+			// expected error
+			return
+		}
+	}
+	if dir != test.Directory {
+		t.Errorf("Incorrect directory: expected %s but got %s",
+			test.Directory, dir)
+	}
+	if tpl != test.Template {
+		t.Errorf("Incorrect template: expected %s but got %s",
+			test.Template, tpl)
+	}
+	os.Args = _Args
+}
+
+func TestArgs(t *testing.T) {
+	testArgs(t, ArgTestCase{
+		Args: []string{"-dir", "testdata", "test.html"},
+		Template: "test.html",
+		Directory: "testdata",
+		Err: false,
+	})
+	testArgs(t, ArgTestCase{
+		Args: []string{"./testdata/test.html"},
+		Template: "./testdata/test.html",
+		Directory: "./",
+		Err: false,
+	})
+	testArgs(t, ArgTestCase{
+		Args: []string{"./testdata/nonexistent"},
+		Template: "./testdata/nonexistent",
+		Directory: "./",
+		Err: false,
+	})
+	testArgs(t, ArgTestCase{
+		Args: []string{},
+		Template: "",
+		Directory: "",
+		Err: true,
+	})
+	testArgs(t, ArgTestCase{
+		Args: []string{"x", "y"},
+		Template: "",
+		Directory: "",
+		Err: true,
 	})
 }
